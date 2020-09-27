fead41db48f2e59f5cdc1fea917e94b9af54af14
Marcus <andersson257@gmail.com>
2020-09-27 14:38:18 +0200

    apa

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,16 +1,17 @@
 import * as firebase from 'firebase';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyBo2FyrSVZnChjKFlPHUoP1HC1CnzeMUXw",
-    authDomain: "expensifybymarcus.firebaseapp.com",
-    databaseURL: "https://expensifybymarcus.firebaseio.com",
-    projectId: "expensifybymarcus",
-    storageBucket: "expensifybymarcus.appspot.com",
-    messagingSenderId: "277342894265",
-    appId: "1:277342894265:web:530f887932a70b35178368"
+    apiKey: process.env.FIREBASE_API_KEY,
+    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+    databaseURL: process.env.FIREBASE_DATABASE_URL,
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.FIREBASE_APP_ID
 };
 
 firebase.initializeApp(firebaseConfig);
 
 const database = firebase.database();
 
+export { firebase, database as default };
